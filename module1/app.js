3dbacb89c5569e777e039db113d7330370d60983
Gwilym Kuiper <gw@ilym.me>
2016-09-08 23:00:19 +0100

    Completed bonus 1

--- module1/app.js
+++ module1/app.js
@@ -8,14 +8,17 @@
   function LunchCheckController($scope) {
     $scope.message = '';
     $scope.lunchItems = '';
+    $scope.messageClass = '';
 
     $scope.checkTooMuch = function () {
       if($scope.lunchItems.trim() === '') {
         $scope.message = 'Please enter data first';
+        $scope.messageClass = 'invalid-entry';
         return;
       }
 
       var numberOfItems = $scope.lunchItems.split(',').length;
+      $scope.messageClass = 'valid-entry';
 
       if(numberOfItems > 3) {
         $scope.message = 'Too Much';
